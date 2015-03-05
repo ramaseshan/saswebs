d12880e014d6fa4d6fc08aa09f8eb082282c0926
ramaseshan <ramaseshanmukund@gmail.com>
2015-03-05 05:39:51 +0000

    Pythonanywhere update

---
+++ assets/js/common.js
@@ -0,0 +1,15 @@
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('.page-scroll a').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+
+
+
+ new WOW().init();
